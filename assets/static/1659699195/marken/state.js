d208892e460c38ffac1109b90588d25ef80efd4b
wassersport24-net <wassersport24-net@users.noreply.github.com>
2022-08-05 14:51:36 +0000

    deploy: fb734926f3882ea5cf8fae18482656cd2b646d00

--- assets/static/1659699195/marken/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fassets\u002Fstatic\u002F1659699195",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmarken\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fassets\u002F",cdnURL:a}}}}(null));
